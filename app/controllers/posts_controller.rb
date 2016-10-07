de5644f49c91e7ae087f9a9247bd5b64904fccdf
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-10-07 16:19:10 -0500

    added posts form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_parms)
+    if @post.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Your post was not saved"
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
