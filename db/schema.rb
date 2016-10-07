b7d9d1ffbf7f5d0e1b8ed40461f9813d627db4cb
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-10-07 16:08:53 -0500

    added post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007210637) do
+ActiveRecord::Schema.define(version: 20161007210823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20161007210637) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_classmates_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_classmates_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "classmate_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
