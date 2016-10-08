288798070b0728fbc9994fe5af9cc195a17e909e
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-10-08 10:11:02 -0500

    add before action to authenticate_classmate in post and pages controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
+  before_action :authenticate_classmate!
+
   def home
     @posts = Post.all
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_classmate!
+
   def new
     @post = Post.new
   end
