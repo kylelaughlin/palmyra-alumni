3095e328935ba32824406b6032363e9ed5b94d67
Kyle Laughlin <kylejlaughlin@icloud.com>
2017-01-09 14:20:13 -0600

    add datatables to graduates

--- app/assets/javascripts/graduates.js
+++ app/assets/javascripts/graduates.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on("turbolinks:load", function(){
+  $("#graduates-table").DataTable({
+    initComplete: function () {
+      var column = this.api().column(1)
+      var select = $('<select><option value=""></option></select>')
+        .appendTo( $(column.header()) )
+        .on( 'change', function() {
+          var val = $.fn.dataTable.util.escapeRegex(
+            $(this).val()
+          );
+          column
+            .search( val ? '^'+val+'$' : '', true, false )
+            .draw();
+        });
+      column.data().unique().sort().each( function ( d, j ) {
+        select.append( '<option value="'+d+'">'+d+'</option>')
+      });
+    }
+  })
+})
